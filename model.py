a1478fb768429df34e6bdb38605361e315d38589
elmanko <elmankisimo@gmail.com>
2020-05-13 18:14:21 +0530

    python file to handle the json db

--- model.py
+++ model.py
@@ -4,4 +4,8 @@ def load_db():
     with open("test_db.json") as t:
         return json.load(t)
 
+def save_db():
+    with open("test_db.json", "w") as t:
+        return json.dump(db,t)
+        
 db = load_db()
