63a04d7afa28fc7d4c78915346a2561f399515c1
elmanko <elmankisimo@gmail.com>
2020-05-13 18:17:07 +0530

    added new functions to handle POST and more json stuff

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
-#from datetime import datetime
-from model import db
+from flask import Flask, render_template,abort,jsonify,request,redirect,url_for
+from datetime import datetime
+from model import db,save_db
 
 app = Flask(__name__)
 
@@ -8,13 +8,17 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     #return "Welcome to my Flask app"
-    return render_template("welcome.html",message='Not sure.')
+    #return render_template("welcome.html",message='Not sure.')
+    return render_template(
+        "welcome.html",
+        querys=db
+    )
 
-"""
+
 @app.route("/date")
 def date():
-    return "The datetime is " + str(datetime.now())
-
+    return jsonify('date time is:', str(datetime.now()))
+"""
 count = 0
 @app.route("/views")
 def views():
@@ -24,6 +28,50 @@ def views():
 """
 
 @app.route("/query/<int:index>")
-def query(index):
-    query=db[index]
-    return render_template("query.html",query=query)
+def query_view(index):
+    try:
+        query=db[index]
+        return render_template("query.html",
+                                query=query, 
+                                index=index,
+                                max_index=len(db)-1)
+    except IndexError:
+        abort(404)
+
+@app.route("/add_query", methods=['GET','POST'])
+def add_query():
+    if request.method == 'POST':
+        #submission
+        query= {"value1": request.form['value1'],
+                "value2": request.form['value2']}
+        db.append(query)
+        save_db()
+        return redirect(url_for('query_view', index=len(db)-1))
+    else:
+        return render_template("add_query.html")
+
+@app.route('/remove_query/<int:index>',methods=['GET','POST'])
+def remove_query(index):
+    try:
+        if request.method == 'POST':
+            del db[index]
+            save_db
+            return redirect(url_for('welcome'))
+        else:
+            return render_template("remove_query.html", query=db[index])
+    except IndexError:
+        abort(404)
+
+#RESTful API test, returning json
+
+@app.route("/api/query/")
+def api_query_list():
+    return jsonify(db)
+
+@app.route("/api/query/<int:index>")
+def api_query_detail(index):
+    try:
+        return jsonify(db[index])
+    except IndexError:
+        abort(404)
+
